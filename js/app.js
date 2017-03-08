3096e83321bc1d89465fe208ccebba0d6f2dd846
gostpeipei <254699093@qq.com>
2017-03-08 11:05:28 +0800

    添加任务

--- js/app.js
+++ js/app.js
@@ -13,9 +13,31 @@
 		var task = [
 			{id:0,name:'抽烟',isCompleted:false},
 			{id:1,name:'喝酒',isCompleted:true},
-			{id:1,name:'烫头',isCompleted:true}
+			{id:2,name:'烫头',isCompleted:true},
+			{id:3,name:'唱歌',isCompleted:false},
+			{id:4,name:'旅游',isCompleted:true}
 		]
 
-		vm.taskList = task
+		vm.taskList = task;
+		vm.newTask = '';
+
+		//判断id
+		var id = 0;
+		var len = vm.taskList.length;
+		if( len === 0 ){
+			id = 1;
+		}else{
+			id = vm.taskList[len-1].id+1;
+		}
+
+		vm.add = function(){
+			if(vm.newTask.trim() === '' ){
+				return;
+			}
+			vm.taskList.push(
+					{id:id,name:vm.newTask,isCompleted:false}
+				)
+			vm.newTask = '';
+		}
 	}
 })(window);
