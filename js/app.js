87d924de99cb7dd6a9760fd171c9bd5835bd46f0
gostpeipei <254699093@qq.com>
2017-03-08 15:25:40 +0800

    全选 清除完成

--- js/app.js
+++ js/app.js
@@ -20,13 +20,15 @@
 
 		vm.taskList = task;
 		vm.newTask = '';
+		vm.allCompleted = false;
+		vm.isShow = false;
 
-
-		var id = 0;
+		//添加功能
 		vm.add = function(){
 			if(vm.newTask.trim() === '' ){
 				return;
 			}
+			var id = 0;
 			//判断id
 			var len = vm.taskList.length;
 			if( len === 0 ){
@@ -38,8 +40,9 @@
 					{id:id,name:vm.newTask,isCompleted:false}
 				)
 			vm.newTask = '';
-			console.log(len)
 		}
+
+		//删除功能
 		vm.remove = function(id){
 			vm.taskList.forEach(function(v,i){
 				if(v.id === id){
@@ -47,11 +50,56 @@
 				}
 			})
 		}
+
+
+		//编辑功能
 		vm.edit = function( id ){
 			vm.editId = id;
 		}
 		vm.upload = function( id ){
 			vm.editId = null;
+		}
+
+
+		//全选功能
+		// 一 change事件方法
+		// vm.allchecked = function(){
+		// 	vm.taskList.forEach(function(v,i){
+		// 		v.isCompleted = vm.allCompleted
+		// 	})
+		// }
+
+		//二  监视
+		vm.$watch('allCompleted',function( newV,oldV ){
+			if( newV === oldV ){
+				return;
+			}
+			vm.taskList.forEach(function(v,i){
+				v.isCompleted = newV
+			})
+		})
+
+		//清除已完成任务
+		//有未完成的任务,就让按钮显示
+		vm.$watch('taskList',function( newV,oldV ){
+			var temp = false;
+			for(var i=0;i<vm.taskList.length;i++){
+				if(vm.taskList[i].isCompleted){
+					temp = true;
+					break;
+				}
+			}
+			vm.isShow = temp;
+		},true)
+		vm.clearCompleted = function(){
+			//保存未完成的任务
+			var temp = [];
+			for(var i=0;i<vm.taskList.length;i++){
+				if( !vm.taskList[i].isCompleted ){
+					temp.push(vm.taskList[i])
+				}
+			}
+			vm.taskList = temp;
 		}
 	}
 })(window);
